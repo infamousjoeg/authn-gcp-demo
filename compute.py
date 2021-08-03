8469ff79842921a0522e77eef26f0b46f83818d8
Joe Garcia <joe@joe-garcia.com>
2021-08-03 17:29:32 -0400

    simplified script for easier reading

--- compute.py
+++ compute.py
@@ -2,38 +2,20 @@
 
 import requests
 
+# Form headers for Google Metadata request
+headers = {
+    'Metadata-Flavor': 'Google',
+}
 
-def urlify(input_str):
-    return input_str.replace('/', '%2F')
+# Urlify host identity to apply to audience claim
+audience_claim = "conjur%2Fcyberarkdemo%2Fhost%2Fgcp%2Fcompute"
 
-
-def get_id_token(audience):
-  header = {
-    'Metadata-Flavor': 'Google'
-  }
-
-  token_response = requests.get('http://metadata/computeMetadata/v1/instance/service/accounts/default/identity?audience={}'.format(audience), headers=header)
-  return token_response.text
-
-
-def conjur_authenticate(url, account, token):
-  header = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Accept-Encoding': 'base64'
-  }
-
-  conjur_token = requests.post(f'{url}/authn-gcp/{account}/authenticate', headers=header)
-  return conjur_token.text
+# Send request and print response
+response = requests.get('http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience={}'.format(audience_claim), headers=headers)
+print("Google Provided JWT: {}".format(response.text))
 
 
-##########
-## Main ##
-##########
 
-if __name__ == "__main__":
-  token = get_id_token(urlify("conjur/cyberarkdemo/host/gcp/compute"))
-  print("Google Provided JWT: {}".format(token))
-  
   #curl -k --request POST \
   #  'https://ec2-0-000-000-00.eu-central-1.compute.amazonaws.com/authn-gcp/myorg/authenticate' --header 'Content-Type: application/x-www-form-urlencoded'\ 
   #  --header "Accept-Encoding: base64" \ 
