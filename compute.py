1498bb59e30487b2138a0484228782a5123930e4
Joe Garcia <joe@joe-garcia.com>
2021-08-03 17:58:58 -0400

    Add Conjur authenticate using authn-gcp

--- compute.py
+++ compute.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import requests
+import json
 
 # Form headers for Google Metadata request
-headers = {
+jwt_headers = {
     'Metadata-Flavor': 'Google',
 }
 
@@ -11,10 +12,21 @@ headers = {
 audience_claim = "conjur%2Fcyberarkdemo%2Fhost%2Fgcp%2Fcompute"
 
 # Send request and print response
-response = requests.get('http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience={}'.format(audience_claim), headers=headers)
-print("Google Provided JWT: {}".format(response.text))
+jwt_response = requests.request('GET', 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience={}'.format(audience_claim), headers=jwt_headers)
+print("Google Provided JWT: {}".format(jwt_response.text))
 
+# Form headers for Conjur authn-gcp authenticate request
+conjur_token_headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept-Encoding': 'base64',
+}
 
+# Form body for Conjur authn-gcp authenticate request
+conjur_token_body = "jwt={}".format(jwt_response.text)
+
+# Send Google Provided JWT to Conjur authn-gcp authenticate endpoint
+conjur_token = requests.request('POST', 'https://dap.joegarcia.dev/authn-gcp/cyberarkdemo/authenticate', headers=conjur_token_headers, data=conjur_token_body)
+print("Conjur Session Token: {}".format(conjur_token.text))
 
   #curl -k --request POST \
   #  'https://ec2-0-000-000-00.eu-central-1.compute.amazonaws.com/authn-gcp/myorg/authenticate' --header 'Content-Type: application/x-www-form-urlencoded'\ 
