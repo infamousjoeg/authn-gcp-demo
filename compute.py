c44eb347c6dc459be7a1e780a22224796b28062b
Joe Garcia <joe@joe-garcia.com>
2021-08-04 11:44:02 -0400

    finalize secret retrieval

--- compute.py
+++ compute.py
@@ -12,7 +12,11 @@ jwt_headers = {
 audience_claim = "conjur%2Fcyberarkdemo%2Fhost%2Fgcp%2Fcompute"
 
 # Send request and print response
-jwt_response = requests.request('GET', 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience={}'.format(audience_claim), headers=jwt_headers)
+jwt_response = requests.request(
+    'GET',
+    'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience={}&format=full'.format(audience_claim),
+    headers=jwt_headers
+)
 print("Google Provided JWT: {}".format(jwt_response.text))
 
 # Form headers for Conjur authn-gcp authenticate request
@@ -23,21 +27,29 @@ conjur_token_headers = {
 
 # Form body for Conjur authn-gcp authenticate request
 conjur_token_body = "jwt={}".format(jwt_response.text)
+print()
+print(conjur_token_body)
 
 # Send Google Provided JWT to Conjur authn-gcp authenticate endpoint
-conjur_token = requests.request('POST', 'https://dap.joegarcia.dev/authn-gcp/cyberarkdemo/authenticate', headers=conjur_token_headers, data=conjur_token_body)
+conjur_token = requests.request(
+    'POST',
+    'https://dap.joegarcia.dev/authn-gcp/cyberarkdemo/authenticate',
+    headers=conjur_token_headers,
+    data=conjur_token_body
+)
+print()
 print("Conjur Session Token: {}".format(conjur_token.text))
 
-  #curl -k --request POST \
-  #  'https://ec2-0-000-000-00.eu-central-1.compute.amazonaws.com/authn-gcp/myorg/authenticate' --header 'Content-Type: application/x-www-form-urlencoded'\ 
-  #  --header "Accept-Encoding: base64" \ 
-  #  --data-urlencode 'jwt=eyJ0e......jjjkl'
+# Form header for Conjur API secret retrieval
+header = {
+    'Authorization': 'Token token={}'.format(conjur_token.text)
+}
 
-  # Send JWT to authn-gcp authenticator in Conjur API
-
-  # Receive JWT Conjur session token in return
-
-  # Retrieve secrets
-
-  # Print secrets
-
+# Retrieve and print gcp/db_password secret variable value
+conjur_variable = requests.request(
+    'GET',
+    'https://dap.joegarcia.dev/secrets/cyberarkdemo/variable/gcp%2Fdb_password',
+    headers=header
+)
+print()
+print("Conjur Secret Variable Value: {}".format(conjur_variable.text))
